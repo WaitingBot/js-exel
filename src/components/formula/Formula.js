b50329ab50c0e3c457cdc96fb2f1a6f4f6a7ce37
grizzly <waitingbot@gmail.com>
2020-05-28 18:24:50 +0700

    18.24

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,9 +1,12 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Formula extends ExcelComponent {
-  // static className = 'excel__formula'
+  static className = 'excel__formula'
 
   toHTML() {
-    return '<h1>Forumla</h1>'
+    return `
+      <div class="info">fx</div>
+      <div class="input" contenteditable spellcheck="false"></div>
+    `
   }
 }
