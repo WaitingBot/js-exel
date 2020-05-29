a18db1c3f076d6a18ec18e7c4657c4b9c11f2c4e
waitingbot <waitingbot@gmail.com>
2020-05-29 12:00:31 +0700

    end making framework

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,7 +1,18 @@
 import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+  }
   toHTML() {
     return '<h1>Forumla</h1>'
+  }
+
+  init() {
+    this.initDOMListeners()
+  }
+
+  destroy() {
+    this.removeDOMListeners()
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -11,9 +11,20 @@ class Dom {
     }
     return this.$el.outerHTML.trim()
   }
+
   clear() {
     this.html('')
+    return this
   }
+
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback)
+  }
+
+  off(eventType, callback) {
+    this.$el.removeEventListener(eventType, callback)
+  }
+
   append(node) {
     if (node instanceof Dom) {
       node = node.$el
